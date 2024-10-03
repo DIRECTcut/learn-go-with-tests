b09ccd2ae9595e746f73a87fc95d8abda5a44789
Sergei A <sergei.afonchenko42@gmail.com>
2024-10-02 21:00:19 -0300

    feat(sum) Implement Sum, SumAll, SumAllTails

--- sum/sum.go
+++ sum/sum.go
@@ -8,3 +8,29 @@ func Sum(input []int) int {
 
 	return result
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var res []int
+
+	for _, slice := range numbersToSum {
+		res = append(res, Sum(slice))
+	}
+
+	return res
+}
+
+func SumAllTails(slicesToSum ...[]int) []int {
+	res := []int{}
+
+	for _, slice := range slicesToSum {
+		if len(slice) == 0 {
+			res = append(res, 0)
+			continue
+		}
+
+		tail := slice[1:]
+		res = append(res, Sum(tail))
+	}
+
+	return res
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -9,9 +12,7 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		want := 10
 
-		if got != want {
-			t.Errorf("Want %d got %d given %v", got, want, numbers)
-		}
+		assertSum(got, want, t, numbers)
 	})
 
 	t.Run("Collection of any size", func(t *testing.T) {
@@ -19,8 +20,61 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		want := 6
 
-		if got != want {
-			t.Errorf("Wanted %d got %d given %v", want, got, numbers)
-		}
+		assertSum(got, want, t, numbers)
 	})
+}
+
+func assertSum(got int, want int, t *testing.T, numbers []int) {
+	if got != want {
+		t.Errorf("Want %d got %d given %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	inputLeft := []int{0, 9}
+	inputRight := []int{1, 2}
+
+	want := []int{9, 3}
+	got := SumAll(inputLeft, inputRight)
+
+	assertSumTails(got, want, t, inputLeft, inputRight)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("Sum non empty slices", func(t *testing.T) {
+		inputLeft := []int{0, 9}
+		inputRight := []int{1, 2}
+		got := SumAllTails(inputLeft, inputRight)
+		want := []int{9, 2}
+
+		assertSumTails(got, want, t, inputLeft, inputRight)
+	})
+
+	t.Run("Sum empty slices", func(t *testing.T) {
+		inputLeft := []int{}
+		inputRight := []int{1, 2}
+		got := SumAllTails(inputLeft, inputRight)
+		want := []int{0, 2}
+
+		assertSumTails(got, want, t, inputLeft, inputRight)
+	})
+
+	t.Run("Sum empty input", func(t *testing.T) {
+		got := SumAllTails()
+		want := []int{}
+
+		assertSumTails(got, want, t, nil, nil)
+	})
+}
+
+func assertSumTails(got []int, want []int, t *testing.T, inputLeft []int, inputRight []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Wanted %v got %v given %v and %v", want, got, inputLeft, inputRight)
+	}
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([]int{1, 2, 3}, []int{2, 3})
+	}
 }
